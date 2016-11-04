77455b43b652605e5dde4c4f4208bc53aaa876c6
v2south <songyin1028@gmail.com>
2016-11-03 17:31:36 -0700

    Update ICM.R

--- R_file/ICM.R
+++ R_file/ICM.R
@@ -1,2 +1,5 @@
 ## Algorithm Implementaion for ICM.
 ## ICM needs to find the mode of conditional distribution.
+## Algorithm and pseudo could be found in the Model_Setup document.
+# Set the values for parameters of Prior. 
+# Need to be careful with variance components, particularly, the parameters for Iverse Gamma distribution.
